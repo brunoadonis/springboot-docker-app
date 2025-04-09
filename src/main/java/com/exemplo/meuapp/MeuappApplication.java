d91ce224c4e544e89e3ec072534a6ac4ba97cfb2
Bruno Adonis <b.adonisrocha@gmail.com>
2025-04-09 09:09:00 -0300

    Deploy inicial do projeto Spring Boot com Docker

---
+++ src/main/java/com/exemplo/meuapp/MeuappApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.meuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuappApplication.class, args);
+	}
+
+}
